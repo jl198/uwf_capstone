da80da462898762367b8bac97d4bac7b70391a7b
Jacob Leiner <jacob.leiner@gmail.com>
2022-03-02 14:36:16 -0800

    initial commit

--- shotstar/shotstar/settings.py
+++ shotstar/shotstar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m4*t!5i11=b3jkew4(!l_e_byq!(2i=q&za(kz+-)c_ocyosju'
+SECRET_KEY = 'django-insecure-7d54e1o+7@^sph)3zz629%r38_jx)qy2gb370l)tzwlbj=-(95'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
